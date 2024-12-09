cbf2a4950c672914a8b7456fc3ee1b4e26e2f240
Seth <blismatic@gmail.com>
2024-12-08 19:50:21 -0800

    solve day 08 part 2

--- 08/main.py
+++ 08/main.py
@@ -34,34 +34,56 @@ def parse(puzzle_input: str) -> dict[tuple[int, int], str]:
     return grid
 
 
-def part1(data):
+def part1(data: dict[tuple[int, int], str]) -> int:
     """Solve and return the answer to part 1."""
-    grid: dict[tuple[int, int], str] = data
-    frequencies = string.ascii_letters + string.digits
-    frequencies = [f for f in frequencies if f in grid.values()]
+    frequencies = [
+        char for char in string.ascii_letters + string.digits if char in data.values()
+    ]
     unique_antinode_positions = set()
 
     for f in frequencies:
-        locations = [
-            pos for pos, char in grid.items() if char == f
+        antennas = [
+            pos for pos, char in data.items() if char == f
         ]  # this will be a list of 2-tuples, like [(1, 3), (4, 2), (7, 7)]
-        for i, loc in enumerate(locations):
+        for i, loc in enumerate(antennas):
             x, y = loc
-            siblings = locations[:i] + locations[i + 1 :]
+            siblings = antennas[:i] + antennas[i + 1 :]
 
             for s in siblings:
                 dx, dy = (x - s[0], y - s[1])
                 antinode_pos = (x + dx, y + dy)
-                print(antinode_pos)
-                if antinode_pos in grid:
+                if antinode_pos in data:
                     unique_antinode_positions.add(antinode_pos)
 
     return len(unique_antinode_positions)
 
 
-def part2(data):
+def part2(data: dict[tuple[int, int], str]):
     """Solve and return the answer to part 2."""
-    pass
+    frequencies = [
+        char for char in string.ascii_letters + string.digits if char in data.values()
+    ]
+    unique_antinode_positions = set()
+
+    for f in frequencies:
+        antennas = [pos for pos, char in data.items() if char == f]
+        unique_antinode_positions.update(
+            antennas
+        )  # Because of resonant harmonics, every antenna is also an antinode
+        for i, loc in enumerate(antennas):
+            x, y = loc
+            siblings = antennas[:i] + antennas[i + 1 :]
+
+            for s in siblings:
+                dx, dy = (x - s[0], y - s[1])
+                antinode_pos = (x + dx, y + dy)
+
+                # Keep moving along the slope as long as the next position is within the grid
+                while antinode_pos in data:
+                    unique_antinode_positions.add(antinode_pos)
+                    antinode_pos = (antinode_pos[0] + dx, antinode_pos[1] + dy)
+
+    return len(unique_antinode_positions)
 
 
 def solve(puzzle_input) -> tuple:

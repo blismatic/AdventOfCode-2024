9cf6bf09a74995d09b0292df96388eaf8822b7b2
Seth <blismatic@gmail.com>
2024-12-11 23:07:11 -0800

    solve day 12 part 1

--- 12/main.py
+++ 12/main.py
@@ -1,9 +1,10 @@
+from collections import deque
 from pprint import pprint
 
 from aocd import get_data
 from dotenv import load_dotenv
 
-with open("12/example2.txt", "r") as infile:
+with open("12/example.txt", "r") as infile:
     example_input = infile.read()
 
 
@@ -11,21 +12,68 @@ def parse(puzzle_input: str):
     """Parse input."""
     result = puzzle_input.split("\n")
 
+    height = len(result)
+    width = len(result[0])
+
+    grid = {}
+    for y in range(height):
+        for x in range(width):
+            grid[(x, height - y - 1)] = result[y][x]
+
     pprint(result[:3])
     print()
-    return result
+    return grid
 
 
-def price(region: list[tuple[int, int]]) -> int:
-    area = 0
-    perimeter = 0
-    return area * perimeter
+def move(pos: tuple[int, int], direction: str) -> tuple[int, int]:
+    dirs = {"N": (0, 1), "E": (1, 0), "S": (0, -1), "W": (-1, 0)}
+
+    x, y = pos
+    dx, dy = dirs[direction]
+
+    return (x + dx, y + dy)
 
 
-def part1(data):
+def part1(data: dict[tuple[int, int], str]) -> int:
     """Solve and return the answer to part 1."""
-    regions = []
-    return sum([price(r) for r in regions])
+    prices = []
+
+    grid = data.copy()
+    already_visited = set()  # This will be a global variable to track which positions we have already processed at some point
+
+    for pos in grid:
+        if pos in already_visited:
+            continue
+
+        region_area = 0
+        region_perimeter = 0
+        q = deque()
+        q.append(pos)
+
+        # Keep exploring as long as the queue is not empty.
+        while q:
+            curr_pos = q.pop()
+            if curr_pos in already_visited:
+                continue
+            else:
+                already_visited.add(curr_pos)  # Add current position so that we won't ever analyze this position again
+                region_area += 1
+
+                neighbors = [move(curr_pos, direction) for direction in ["N", "E", "S", "W"]]
+                for n in neighbors:
+                    if n in grid and grid[n] == grid[curr_pos]:  # If the neighbor is a valid position *and* of the same plant type
+                        q.append(
+                            n
+                        )  # Add this neighboring position to the queue, so that it will also be explored (since it must be part of the same region)
+                    else:
+                        region_perimeter += 1
+
+        # After the queue is empty / exhausted, we know that the region's area/perimeter must be fully explored
+        region_price = region_area * region_perimeter
+        prices.append(region_price)
+
+    result = sum(prices)
+    return result
 
 
 def part2(data):
@@ -44,8 +92,8 @@ def solve(puzzle_input) -> tuple:
 
 if __name__ == "__main__":
     load_dotenv()
-    solutions = solve(example_input)
+    # solutions = solve(example_input)
     puzzle_input = get_data(day=12, year=2024)
-    # solutions = solve(puzzle_input)
+    solutions = solve(puzzle_input)
 
     print("\n".join(str(solution) for solution in solutions))

5a3267ab53a8251b7b62827213d2a054159eeda0
Seth <blismatic@gmail.com>
2024-12-17 22:43:33 -0800

    solve day 18 part 2

--- 18/main.py
+++ 18/main.py
@@ -4,8 +4,9 @@ import networkx as nx
 from aocd import get_data
 from dotenv import load_dotenv
 
-# SIZE = 7
 SIZE = 71
+SRC = (0, 0)
+TRGT = (SIZE - 1, SIZE - 1)
 
 with open("18/example_input.txt", "r") as infile:
     example_input = infile.read()
@@ -40,8 +41,6 @@ def part1(data: list[tuple[int, int]]) -> int:
     for pos in data[:1024]:
         grid[pos] = "#"
 
-    # pprint(grid)
-
     G = nx.Graph()
     for pos, char in grid.items():
         if char == ".":
@@ -50,14 +49,37 @@ def part1(data: list[tuple[int, int]]) -> int:
                 if n in grid and grid[n] == ".":
                     G.add_edge(pos, n)
 
-    result = nx.shortest_path_length(G, source=(0, 0), target=(SIZE - 1, SIZE - 1))
+    result = nx.shortest_path_length(G, source=SRC, target=TRGT)
 
     return result
 
 
-def part2(data):
+def part2(data: list[tuple[int, int]]) -> str:
     """Solve and return the answer to part 2."""
-    pass
+
+    grid = {}
+    for x in range(SIZE):
+        for y in range(SIZE):
+            grid[(x, y)] = "."
+
+    # Create graph by adding edges
+    G = nx.Graph()
+    for pos in grid:
+        neighbors = [move(pos, d) for d in ["N", "E", "S", "W"]]
+        for n in neighbors:
+            if n in grid:
+                G.add_edge(pos, n)
+
+    # Continually remove one node at a time and check that there is a valid path
+    for pos in data:
+        grid[pos] = "#"
+
+        G.remove_node(pos)
+        try:
+            nx.shortest_path_length(G, source=SRC, target=TRGT)
+        except nx.exception.NetworkXNoPath:
+            x, y = pos
+            return ",".join([str(x), str(y)])
 
 
 def solve(puzzle_input) -> tuple:
